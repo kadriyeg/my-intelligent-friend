e0af9b297120a26e653e1a8c1e5b22dc9fd6ad0c
kadriyeg <kadriye.sinemuse@gmail.com>
2023-12-30 17:27:25 +0300

    entrance component created

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,16 @@
 'use client';
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from  "./pages/home/Home";
-import 'bootstrap/dist/css/bootstrap.css';
-import Navbar from "./components/navbar/Navbar";
+import 'bootstrap/dist/css/bootstrap.css';import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
   return (
     <div>
      
       <BrowserRouter>
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-    
-      </Routes>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
