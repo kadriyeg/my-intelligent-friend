8b570d33c6bef7a449a91a5283f35d2b79a027e4
kadriyeg <kadriye.sinemuse@gmail.com>
2023-12-30 14:41:21 +0300

    router added

---
+++ src/app/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import Navbar from '../../components/navbar/Navbar';
+
+const HomePage = () => {
+    return (
+        <div>
+            deneme
+           
+        </div>
+    );
+};
+
+
+export default HomePage;
