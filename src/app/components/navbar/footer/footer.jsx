c19c9c76c4313ed3278792f4a17c89526adc84df
kadriyeg <kadriye.sinemuse@gmail.com>
2023-12-30 14:02:52 +0300

    home, navbar and footer created

---
+++ src/app/components/navbar/footer/footer.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div>
+      <h1>Welcome to my website!</h1>
+      <p>This is the footer section.</p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/app/components/navbar/navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>Welcome to my App</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
